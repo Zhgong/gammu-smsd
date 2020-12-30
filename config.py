84108f08cf054420217a5a66e847cc42eb38c765
zhou <zhou@ubuntu.com>
2020-12-30 16:08:56 +0100

    add dotenv

---
+++ config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+TOKEN = os.getenv("TOKEN")# sms bot
+CHAT_ID = os.getenv("CHAT_ID")
+
+FOLDERS={
+    # 'inbox':'/home/pi/smsutil/data/inbox',
+    # 'archieve':'/home/pi/smsutil/data/archieve',
+    'inbox':'/var/spool/gammu/inbox/',
+    'archieve':'/var/spool/gammu/archieve'
+    # 'inbox':'/home/zhou/Projects/smsutil/data/inbox',
+    # 'archieve':'/home/zhou/Projects/smsutil/data/archieve',
+}
