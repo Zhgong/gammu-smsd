7fb3288dc86a0092af3d789659bf8c4690ccafc7
zhou <zhou@ubuntu.com>
2021-01-02 00:34:20 +0100

    remove comment

--- config.py
+++ config.py
@@ -7,10 +7,6 @@ TOKEN = os.getenv("TOKEN")  # sms bot
 CHAT_ID = os.getenv("CHAT_ID")
 
 FOLDERS = {
-    # 'inbox':'/home/pi/smsutil/data/inbox',
-    # 'archieve':'/home/pi/smsutil/data/archieve',
     'inbox': '/var/spool/gammu/inbox/',
     'archieve': '/var/spool/gammu/archieve'
-    # 'inbox':'/home/zhou/Projects/smsutil/data/inbox',
-    # 'archieve':'/home/zhou/Projects/smsutil/data/archieve',
 }
