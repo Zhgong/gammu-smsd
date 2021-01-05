1f14fc0c2a91ff653c5a7f5e634623c595d344a2
ubuntu <ubuntu@ubuntu-pi>
2021-01-06 00:41:33 +0100

    add restart

--- scripts/run-production.sh
+++ scripts/run-production.sh
@@ -3,9 +3,10 @@ echo Starting ...
 REL_DIR=$(dirname $(dirname $0))
 echo $REL_DIR
 DIR=$(realpath $REL_DIR)
-docker run -it -d --env-file .env \
+docker run -d --env-file .env \
 --device=/dev/ttyUSB0 \
 --mount type=bind,source=$DIR/gammu-data,target=/var/spool/gammu \
 --mount type=bind,source=$DIR/gammu-smsd.log,target=/var/log/gammu-smsd.log \
 --mount type=bind,source=$DIR/receive-sms.log,target=/var/log/receive-sms.log \
+--restart=always \
 --name gammu-smsd gammu-smsd:latest
